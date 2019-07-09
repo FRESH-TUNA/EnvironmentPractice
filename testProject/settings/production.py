ec5bf5f1b03a13741a1e81dff8b065473ca2c673
DONGWON KIM <lunacircle4@gmail.com>
2019-07-09 22:41:27 +0900

    staticfile 수정

--- testProject/settings/production.py
+++ testProject/settings/production.py
@@ -4,9 +4,9 @@ from django.core.management.utils import get_random_secret_key
 
 ALLOWED_HOSTS = ['tuna-production-practice.pje89ijymw.ap-northeast-2.elasticbeanstalk.com']
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = get_random_secret_key()
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
